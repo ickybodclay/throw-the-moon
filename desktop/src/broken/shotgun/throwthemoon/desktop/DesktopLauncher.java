64e9674bce2a6d4c10d396bcab8a5842eb68743e
Jason Petterson <jp@brokenshotgun.com>
2015-04-20 11:45:26 -0400

    updated desktop config

--- desktop/src/broken/shotgun/throwthemoon/desktop/DesktopLauncher.java
+++ desktop/src/broken/shotgun/throwthemoon/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import broken.shotgun.throwthemoon.ThrowTheMoonGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width=1280;
+		config.height=720;
+		config.vSyncEnabled=true;
 		new LwjglApplication(new ThrowTheMoonGame(), config);
 	}
 }
