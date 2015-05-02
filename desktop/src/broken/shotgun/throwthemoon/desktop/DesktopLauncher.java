f550361ea1b952c3a71bf8ccc885c9d0526584ec
Jason Petterson <jp@brokenshotgun.com>
2015-05-02 11:41:45 -0400

    locked window size to 1280x720

--- desktop/src/broken/shotgun/throwthemoon/desktop/DesktopLauncher.java
+++ desktop/src/broken/shotgun/throwthemoon/desktop/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		config.width=1280;
 		config.height=720;
 		config.vSyncEnabled=true;
+		config.resizable = false;
 		new LwjglApplication(new ThrowTheMoonGame(true), config);
 	}
 }
