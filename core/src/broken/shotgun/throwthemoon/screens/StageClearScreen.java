6339eeaa3b931a4306728f3507b0397b9e36ee49
Jason Petterson <jp@brokenshotgun.com>
2015-05-06 23:04:00 -0400

    added text to background image to replace dynamic text on stage clear
    left in font stuff for when a font exists, commented out for DevFest
    milestone

--- core/src/broken/shotgun/throwthemoon/screens/StageClearScreen.java
+++ core/src/broken/shotgun/throwthemoon/screens/StageClearScreen.java
@@ -7,17 +7,12 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.TextureLoader;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
-import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public class StageClearScreen implements Screen {
@@ -26,17 +21,17 @@ public class StageClearScreen implements Screen {
     
 	private final AssetManager manager;
 	private final Stage stage;
-	private final Batch uiBatch;
+	//private final Batch uiBatch;
 	
 	private static final String TEXTURE_FILENAME = "stageclearbg.png";
 	private Texture background;
-	private BitmapFont font;
-	private static final String STAGE_CLEAR_MESSAGE = "Stage Clear!";
+	//private BitmapFont font;
+	//private static final String STAGE_CLEAR_MESSAGE = "Stage Clear!";
 	
 	public StageClearScreen(final ThrowTheMoonGame game) {
         this.manager = new AssetManager();
         this.stage = new Stage(new StretchViewport(WIDTH, HEIGHT));
-        this.uiBatch = new SpriteBatch();
+        //this.uiBatch = new SpriteBatch();
         
         manager.setLoader(Texture.class, new TextureLoader(new InternalFileHandleResolver()));
         manager.load(TEXTURE_FILENAME, Texture.class);
@@ -46,8 +41,8 @@ public class StageClearScreen implements Screen {
         Image backgroundImg = new Image(background);
         stage.addActor(backgroundImg);
         
-        font = new BitmapFont();
-        font.setColor(Color.WHITE);
+        //font = new BitmapFont();
+        //font.setColor(Color.WHITE);
         
         Gdx.input.setInputProcessor(stage);
         
@@ -76,9 +71,9 @@ public class StageClearScreen implements Screen {
         stage.draw();
         stage.act(delta);
         
-        uiBatch.begin();
-        font.draw(uiBatch, STAGE_CLEAR_MESSAGE, 0, Gdx.graphics.getHeight() * 0.75f, Gdx.graphics.getWidth(), Align.center, true);
-        uiBatch.end();
+        //uiBatch.begin();
+        //font.draw(uiBatch, STAGE_CLEAR_MESSAGE, 0, Gdx.graphics.getHeight() * 0.75f, Gdx.graphics.getWidth(), Align.center, true);
+        //uiBatch.end();
 	}
 
 	@Override
@@ -105,7 +100,7 @@ public class StageClearScreen implements Screen {
 	public void dispose() {
 		manager.dispose();
         stage.dispose();
-        font.dispose();
+        //font.dispose();
 	}
 
 }
