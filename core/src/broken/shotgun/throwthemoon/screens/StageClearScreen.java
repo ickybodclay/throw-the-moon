5a09c876fa65632949dd55d3cb1d4a1526df0ffa
Jason Petterson <jp@brokenshotgun.com>
2015-05-08 23:12:40 -0400

    added stage clear music

--- core/src/broken/shotgun/throwthemoon/screens/StageClearScreen.java
+++ core/src/broken/shotgun/throwthemoon/screens/StageClearScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.TextureLoader;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -21,41 +22,40 @@ public class StageClearScreen implements Screen {
     
 	private final AssetManager manager;
 	private final Stage stage;
-	//private final Batch uiBatch;
 	
 	private static final String TEXTURE_FILENAME = "stageclearbg.png";
+	private static final String MUSIC_FILENAME = "dotty.mp3";
 	private Texture background;
-	//private BitmapFont font;
-	//private static final String STAGE_CLEAR_MESSAGE = "Stage Clear!";
+	private Music music;
 	
 	public StageClearScreen(final ThrowTheMoonGame game) {
         this.manager = new AssetManager();
         this.stage = new Stage(new StretchViewport(WIDTH, HEIGHT));
-        //this.uiBatch = new SpriteBatch();
         
         manager.setLoader(Texture.class, new TextureLoader(new InternalFileHandleResolver()));
         manager.load(TEXTURE_FILENAME, Texture.class);
+		manager.load(MUSIC_FILENAME, Music.class);
         manager.finishLoading();
 
         background = manager.get(TEXTURE_FILENAME);
+		music = manager.get(MUSIC_FILENAME);
+
         Image backgroundImg = new Image(background);
         stage.addActor(backgroundImg);
-        
-        //font = new BitmapFont();
-        //font.setColor(Color.WHITE);
         
         Gdx.input.setInputProcessor(stage);
         
         stage.addListener(new ActorGestureListener() {
-
 			@Override
 			public void tap(InputEvent event, float x, float y, int count, int button) {
+				music.stop();
 				game.setScreen(new GameScreen(game));
-				
 				super.tap(event, x, y, count, button);
 			}
-        	
         });
+
+		music.setLooping(true);
+		music.play();
 	}
 
 	@Override
@@ -70,10 +70,6 @@ public class StageClearScreen implements Screen {
 
         stage.draw();
         stage.act(delta);
-        
-        //uiBatch.begin();
-        //font.draw(uiBatch, STAGE_CLEAR_MESSAGE, 0, Gdx.graphics.getHeight() * 0.75f, Gdx.graphics.getWidth(), Align.center, true);
-        //uiBatch.end();
 	}
 
 	@Override
@@ -100,7 +96,6 @@ public class StageClearScreen implements Screen {
 	public void dispose() {
 		manager.dispose();
         stage.dispose();
-        //font.dispose();
 	}
 
 }
